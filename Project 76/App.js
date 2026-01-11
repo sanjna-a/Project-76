2962e8c9acbe64cb717fee429f8eb2ba264bf335
sanjna-a <sanjna.s.akula@gmail.com>
2026-01-11 18:46:14 -0500

    git.commit

--- Project 76/App.js
+++ Project 76/App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-//import { StackNavigator } from '@react-navigation/stack';
 import { createStackNavigator } from '@react-navigation/stack';
 //import { createStackNavigation } from '@react-navigation/stack';
 //import { createStack } from '@react-navigation/stack';
